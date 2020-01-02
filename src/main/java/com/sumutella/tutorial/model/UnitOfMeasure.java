48c5627113ab64ec97c0d4fbe8c785361496746d
Semir Umut Kurt <fatih.ek96@hotmail.com>
2020-01-02 15:47:53 +0300

    models are created typo fixed. Closes #2

--- src/main/java/com/sumutella/tutorial/model/UnitOfMeasure.java
+++ src/main/java/com/sumutella/tutorial/model/UnitOfMeasure.java
@@ -1,43 +1,21 @@
 package com.sumutella.tutorial.model;
 
+
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.Entity;
 
 /**
  * @author sumutella
- * @time 3:38 PM
+ * @time 3:45 PM
  * @since 1/2/2020, Thu
  */
-
+@Entity
 @Getter
 @Setter
-@Entity
-public class Recipe extends BaseEntity {
+public class UnitOfMeasure extends BaseEntity {
     private String description;
-    private Integer prepTime;
-    private Integer cookTime;
-    private Integer servings;
-    private String source;
-    private String url;
-    @Lob
-    private String directions;
-    @Lob
-    private Byte[] image;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    private Note note;
-
-    @Enumerated(value = EnumType.STRING)
-    private Difficulty difficulty;
-
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private List<Ingredient> ingredients = new ArrayList<>();
-
-    @ManyToMany
-    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private List<Category> categories = new ArrayList<>();
 }
+
+
