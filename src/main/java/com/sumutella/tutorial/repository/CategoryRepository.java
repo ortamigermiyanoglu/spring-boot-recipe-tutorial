c3892fdbca4b32bd4e170da91acb66da39a6b989
Semir Umut Kurt <fatih.ek96@hotmail.com>
2020-01-02 22:20:22 +0300

    data loader  implemented. Closes #6

--- src/main/java/com/sumutella/tutorial/repository/CategoryRepository.java
+++ src/main/java/com/sumutella/tutorial/repository/CategoryRepository.java
@@ -3,10 +3,13 @@ package com.sumutella.tutorial.repository;
 import com.sumutella.tutorial.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * @author sumutella
  * @time 9:55 PM
  * @since 1/2/2020, Thu
  */
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
