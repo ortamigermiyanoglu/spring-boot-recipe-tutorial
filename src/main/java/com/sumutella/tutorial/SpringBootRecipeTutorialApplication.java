585629d914a395f6c433e56a80be882733fe7b7d
Semir Umut Kurt <fatih.ek96@hotmail.com>
2020-01-02 10:40:47 +0300

    initial commit

---
+++ src/main/java/com/sumutella/tutorial/SpringBootRecipeTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.sumutella.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecipeTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRecipeTutorialApplication.class, args);
+    }
+
+}
